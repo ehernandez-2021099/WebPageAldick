419e1b03c8e2f0a20edf6174d9a857e38adb3b7f
dick <ehernandez-2021099@kinal.edu.gt>
2023-01-20 09:15:03 -0600

    Primer commit del proyecto

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function saludar(){
+ let num1 = document.getElementById('valor1').value;
+ let num2 = document.getElementById('valor2').value;
+
+ let resultadi = parseInt(num1) + parseInt(num2);
+ console.log('Saludos')
+ console.log(resultadi)
+}
+
